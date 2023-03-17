b6b9811c6f300a5a6a0fffe8b4acaf12276859e6
ithyx <lamidey.m@gmail.com>
2023-03-17 17:49:12 -0400

    Re-organized scenes and assets

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import mitsuba as mi
 
 mi.set_variant('cuda_ad_rgb')
 
-img = mi.render(mi.load_file("scenes/cbox.xml"), spp=256)
+img = mi.render(mi.load_file("scenes/base/cbox.xml"), spp=256)
 
 mi.Bitmap(img).write('out.exr')
