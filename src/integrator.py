90ef86c458fc8bca34818a550e8cf03d85ad42de
ithyx <lamidey.m@gmail.com>
2023-05-03 17:16:33 -0400

    Added density param and started work on transmittance

--- src/integrator.py
+++ src/integrator.py
@@ -1,4 +1,5 @@
 from typing import cast, Optional
+from math import exp
 
 import drjit as dr
 import mitsuba as mi
@@ -7,8 +8,11 @@ class GodraysIntegrator(mi.SamplingIntegrator):
     def __init__(self, props: mi.Properties) -> None:
         super().__init__(props)
 
-        self.input_light = cast(mi.Point3f, cast(mi.Emitter, props.get('input_light')).bbox().center())
+        light_data = cast(mi.Emitter, props.get('input_light'))
+
+        self.input_light_pos = cast(mi.Point3f, light_data.bbox().center())
         self.step_count = cast(int, props.get('step_count', 20))
+        self.density = cast(float, props.get('density', 0.7))
 
     def sample(self, scene: mi.Scene, sampler: mi.Sampler, ray: mi.RayDifferential3f, medium: Optional[mi.Medium] = None, active: bool = True ) -> tuple[mi.Color3f, bool, list[float]]:
         color = dr.zeros(mi.Color3f, shape=1)
@@ -28,7 +32,15 @@ class GodraysIntegrator(mi.SamplingIntegrator):
             for step in dr.linspace(dtype=dr.llvm.ad.ArrayXf, start=step_distance, stop=distance, num=self.step_count, endpoint=False):
                 # Génération d'un rayon vers la lumière sélectionnée à partir du step
                 scatter_origin = ray2(step)
-                scatter_dir = self.input_light - scatter_origin
+                scatter_dir = self.input_light_pos - scatter_origin # type: ignore (Is this really doing what I think it is?)
                 scatter_ray = mi.Ray3f(scatter_origin, scatter_dir)
+                scatter_its = cast(mi.SurfaceInteraction3f, scene.ray_intersect(scatter_ray))
+
+                # TODO: check for occlusion
+                # TODO: make sure thos works for light inside the volume
+
+                # Calcul de la transmittance à partir de la densité (homogène)
+                # https://www.pbr-book.org/3ed-2018/Light_Transport_II_Volume_Rendering/Sampling_Volume_Scattering#HomogeneousMedium
+                transmittance = exp(-(self.density * scatter_its.t))
 
         return (color, True, [])
