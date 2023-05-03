34b068da6f2febba7a0c4c258dc23baa1de42c8e
ithyx <lamidey.m@gmail.com>
2023-05-03 14:17:32 -0400

    Finally getting an output with custom integrator

--- src/integrator.py
+++ src/integrator.py
@@ -1,12 +1,23 @@
 from typing import Optional
 
-import mitsuba as mi
 import drjit as dr
+import mitsuba as mi
 
-class GodraysIntegrator(mi.MonteCarloIntegrator):
-    def __init__(self, props: mi.Properties):
-        mi.MonteCarloIntegrator.__init__(self, props)
+class GodraysIntegrator(mi.SamplingIntegrator):
+    def __init__(self, props: mi.Properties) -> None:
+        super().__init__(props)
 
     def sample(self, scene: mi.Scene, sampler: mi.Sampler, ray: mi.RayDifferential3f, medium: Optional[mi.Medium] = None, active: bool = True ) -> tuple[mi.Color3f, bool, list[float]]:
+        color = dr.zeros(mi.Color3f)
 
-        return (color, escaped, [])
+        # Intersection initiale
+        its: mi.SurfaceInteraction3f = scene.ray_intersect(ray)
+
+        if (dr.any(its.is_valid())):
+            # Intersection de sortie ou contre une surface dans le volume
+            ray2 = its.spawn_ray(ray.d)
+            its2 = scene.ray_intersect(ray2)
+
+            color[its2.is_valid()] = mi.Color3f(its2.t/4, 0.0, 0.0)
+
+        return (color, True, [])
--- src/main.py
+++ src/main.py
@@ -4,16 +4,22 @@ mi.set_variant("llvm_ad_rgb")
 
 from integrator import GodraysIntegrator
 
+import matplotlib.pyplot as plt
+
 # autopep8: on
 
 
 def main():
     mi.register_integrator("godrays", lambda props: GodraysIntegrator(props))
+    params = {}
+    params["integrator"] = "godrays"
 
-    scene = mi.load_file("scenes/modified/cbox.xml")
-    image = mi.render(scene, spp=256)
+    scene = mi.load_file("scenes/modified/simple.xml", **params)
+    image = mi.render(scene, spp=128)
 
-    mi.util.write_bitmap("out.exr", image)
+    plt.axis('off')
+    plt.imshow(image ** (1.0 / 2.2))
+    plt.show()
 
 
 if __name__ == "__main__":
